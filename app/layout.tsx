9aad307196fa95d01809b10d4ed5283d08f2eec5
aortiz13 <serviciosmensualesadrian@gmail.com>
2026-01-27 09:28:31 -0300

    Remove DebugBanner to fix server render error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter, Outfit, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
-import { DebugBanner } from "@/components/debug/DebugBanner";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const outfit = Outfit({ subsets: ["latin"], variable: "--font-outfit" });
@@ -32,7 +31,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <DebugBanner />
+        >
           {children}
           <Toaster />
         </ThemeProvider>
